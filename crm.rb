18b58650247b6bedf392001be2968a9359561ef1
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-04-12 22:35:18 -0400

    added html form and new route contact edit

--- crm.rb
+++ crm.rb
@@ -38,7 +38,15 @@ get "/contacts/:id" do
   else
     raise Sinatra::NotFound
   end
+end
 
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 # 7 HTTP RESTful Routes
